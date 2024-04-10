041560ab5439b22edd0d0c9436cad02bd9b51020
Anastasios Chatzialexiou <16361161+tasxatzial@users.noreply.github.com>
2024-04-10 11:45:52 +0300

    add basic html and dice state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import Die from './Die.jsx';
+import { nanoid } from 'nanoid';
 
 export default function App() {
-  
+  const [dice, setDice] = React.useState(initializeDice());
+
+  function genNewDie() {
+    return {
+      id: nanoid(),
+      value: Math.floor(Math.random() * 6) + 1
+    }
+  }
+
+  function initializeDice() {
+    const dice = [];
+    for (let i = 0; i < 10; i++) {
+      dice.push(genNewDie());
+    }
+    return dice;
+  }
+
+  const dieComponents = dice.map(die => <Die key={die.id} value={die.value}/>)
+
+  return (
+    <main>
+      <h1>Tenzies</h1>
+      <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <div>
+        {dieComponents}
+      </div>
+      <button>Roll</button>
+    </main>
+  )
 }
